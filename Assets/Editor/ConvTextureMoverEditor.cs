ef281291c0bee4717d822851fddf2e45572372c6
ryevdokimov <robert.yevdokimov@autStand.com>
2023-04-18 20:03:21 -0400

    Fixed Conveyor Scaling Issue

--- Assets/Editor/ConvTextureMoverEditor.cs
+++ Assets/Editor/ConvTextureMoverEditor.cs
@@ -3,14 +3,15 @@
 using UnityEngine;
 using UnityEditor;
 
-[CustomEditor(typeof(ConvTextureMover))]
-public class ConvTextureMoverEditor : Editor
+
+[CustomEditor(typeof(Conveyor))]
+public class ConveyorTextureMoverEditor : Editor
 {
     void OnSceneGUI()
     {
         //DrawDefaultInspector();
-        var player = (ConvTextureMover)target;
-        player.RemakeMesh();
+        var player = (Conveyor)target;
+        player.GetComponentInChildren<ConvTextureMover>().RemakeMesh();
     }
 }
-#endif
+#endif  
